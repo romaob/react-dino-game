182ab03ea9727e01cc4c716a21b0f7c693e61461
Romao Barbosa <romaolb@outlook.com>
2023-09-24 00:29:28 -0300

    first animation test

---
+++ src/game/DinoTest.tsx
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react'
+
+const UPDATE_BODY_RATE = 2000
+const UPDATE_RUN_RATE = 100;
+
+const STOP = false;
+
+export default function DinoTest({
+    time
+}: {
+    time: number;
+}) {
+    const [runUpdate, setRunUpdate] = useState(0);
+    const [bodyUpdate, setBodyUpdate] = useState(0);
+    const [stepRun, setStepRun] = useState(0);
+    const [stepBody, setStepBody] = useState(0);
+
+    const body = require('../assets/dino_body_0.png');
+
+    const headAA = require('../assets/dino_head_0.png');
+    const headAB = require('../assets/dino_head_0b.png');
+    const headAC = require('../assets/dino_head_0c.png');
+    const headBA = require('../assets/dino_head_1.png');
+    const headBB = require('../assets/dino_head_1b.png');
+    const headBC = require('../assets/dino_head_1c.png');
+
+    const handsA = require('../assets/dino_hands_0.png');
+    const handsB = require('../assets/dino_hands_1.png');
+
+    const tailA = require('../assets/dino_tail_0.png');
+    const tailB = require('../assets/dino_tail_1.png');
+
+    const legA = require('../assets/dino_leg_0.png');
+    const legB = require('../assets/dino_leg_1.png');
+    const legC = require('../assets/dino_leg_2.png');
+    const legD = require('../assets/dino_leg_3.png');
+    const legE = require('../assets/dino_leg_4.png');
+    const legF = require('../assets/dino_leg_5.png');
+    const legG = require('../assets/dino_leg_6.png');
+    const legH = require('../assets/dino_leg_7.png');    
+
+
+    const [headImage, setHeadImage] = useState(headBA);
+    const [handsImage, setHandsImage] = useState(handsB);
+    const [tailImage, setTailImage] = useState(tailA);
+
+    const [legImageA, setLegImageA] = useState(legA);
+    const [legImageB, setLegImageB] = useState(legF);
+
+    useEffect(() => {
+        if (STOP) {
+            return;
+        }
+        if (bodyUpdate > UPDATE_BODY_RATE) {
+            if (stepBody === 0) {
+                setHeadImage(headAA);
+            } else if (stepBody === 1) {
+                setHeadImage(headAB);
+            }
+            setStepBody(stepBody === 0 ? 1 : 0);
+            setHandsImage(stepBody > 0 ? handsA : handsB);
+            setBodyUpdate(0);
+            return;
+        }
+        if (runUpdate > UPDATE_RUN_RATE) {
+            let nextStep = stepRun + 1;
+            switch (nextStep) {
+                case 0:
+                    setLegImageA(legA);
+                    setLegImageB(legE);
+                    break;
+                case 1:
+                    setLegImageA(legB);
+                    setLegImageB(legF);
+                    break;
+                case 2:
+                    setLegImageA(legC);
+                    setLegImageB(legG);
+                    break;
+                case 3:
+                    setLegImageA(legD);
+                    setLegImageB(legH);
+                    break;
+                case 4:
+                    setLegImageA(legE);
+                    setLegImageB(legA);
+                    nextStep = 0;
+                    break;
+            }
+            setStepRun(nextStep);
+            setRunUpdate(0);
+            return;
+        }
+        setRunUpdate(runUpdate + time);  
+        setBodyUpdate(bodyUpdate + time);
+    }, [time])
+
+  return (
+    <>
+        <h2>{stepBody} - {stepRun} - {runUpdate}</h2>
+        <div style={styles.dinoMain}>
+            <img style={styles.dinoPart} src={body} alt='body' />
+            <img style={styles.dinoPart} src={tailImage} alt='tail' />
+            <img style={styles.dinoPart} src={headImage} alt='head' />
+            <img style={styles.dinoPart} src={handsImage} alt='hands' />
+            <img style={styles.dinoPart} src={legImageA} alt='leg' />
+            <img style={styles.dinoPart} src={legImageB} alt='leg' />  
+        </div> 
+    </>
+  )
+}
+// Define a type for your styles object
+type Styles = {
+    dinoMain: React.CSSProperties;
+    dinoPart: React.CSSProperties;
+  };
+  
+const styles: Styles = {
+    //All images inside dinoMain should be overlayed on top of each other.
+    dinoMain: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+    },
+    dinoPart: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+    },
+}
--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Background from './Background';
+import DinoTest from './DinoTest';
 
 function Game() {
   // State variables for game data.
   const [gameState, setGameState] = useState(/* initial game state */);
+  const [time, setTime] = useState(0);
+  const [lastTime, setLastTime] = useState(0); 
 
   // Update game state and render the game.
   const gameLoop = useCallback(
@@ -15,27 +18,32 @@ function Game() {
         // Use the game state to render game elements.
     
         // Request the next frame.    
-        console.log('gameLoop...')  
+        if (lastTime !== 0) {
+
+            setTime(new Date().getTime() - lastTime);
+        } else {
+            setLastTime(new Date().getTime());
+        }
         requestAnimationFrame(gameLoop);
     },
-    [],
+    [lastTime, time],
   );
 
   // Start the game loop when the component mounts.
   useEffect(() => {
     requestAnimationFrame(gameLoop);
-    console.log('requestAnimationFrame...');
 
     // Clean up the game loop when the component unmounts.
     return () => {
       // Cancel any pending animation frames.
-      console.log('Unmounting...');
     };
   }, [gameLoop]);
 
   return (
     <div>
         <Background />
+        <DinoTest time={time} />
+        <div style={{width: '95%', height: 2, backgroundColor: 'black', position: 'absolute', top: '55%', left: 0}}></div>
     </div>
   );
 }
